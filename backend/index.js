d11cee2dde5388d457e3e962215394a163963a96
Hasnain911 <126194428+Hasnain911@users.noreply.github.com>
2023-08-26 14:24:35 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,23 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const port = 5000;
-const cors = require("cors")
+const cors = require("cors");
 const mongoDB = require("./db");
 mongoDB();
+
 app.use(cors({
-  origin:["https://64e942355e3821614de87d5a--comfy-lokum-8e7426.netlify.app/"],
-  methods :["POST" ,"GET" ],
-  credentials : "true"
-}))
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-
+  origin: ["https://64e942355e3821614de87d5a--comfy-lokum-8e7426.netlify.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", require("./routes/CreateUser"));
 app.use("/api", require("./routes/DisplayData"));
 app.use("/api", require("./routes/OrderData"));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+

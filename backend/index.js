72301bc7b5a4ff07c35ae568b4ec61374bc86bc4
Hasnain911 <126194428+Hasnain911@users.noreply.github.com>
2023-08-26 13:18:58 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,7 @@ app.use((req, res, next) => {
 });
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", require("./routes/CreateUser"));
